978e88b32f261e06508ee350de9b7041fa55fb10
JubairBinSayeed <jubairbinsayeed@gmail.com>
2024-01-20 11:55:01 +0600

    done upper section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}']
+}
+module.exports = {
+    theme: {
+      extend: {
+        gap: {
+          '11': '9.75rem',
+        }
+      }
+    }
+  }
